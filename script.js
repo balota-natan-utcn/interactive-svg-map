87f0e4f727e90250088cd82f11f432386eb86939
balota-natan-utcn <balotann61@gmail.com>
2025-06-10 17:14:25 +0300

    added on page selected part text & highlight on click

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function showPart(partName)
+let lastClickedSvgPath = null;
+
+function showPart(partName, clickedPathElement)
 {
+  //update displayed part name on the page
+  const selectedPartNameSpan = document.getElementById('selectedPartName');
+  selectedPartNameSpan.textContent = partName.charAt(0).toUpperCase() + partName.slice(1);
+
   console.log(`You chose ${partName}`);
+
+  //remove highlight from previously clicked part, if any
+  if (lastClickedSvgPath)
+  {
+    lastClickedSvgPath.classList.remove('active-cow-part');
+  }
+
+  //add highlight to the newly clicked part
+  clickedPathElement.classList.add('active-cow-part');
+
+  //store the reference to the current clicken part for next time
+  lastClickedSvgPath = clickedPathElement;
 }
